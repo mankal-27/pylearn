eb5d00b484e2cf83b26e75585bb357f3da5c9074
mankal-27 <mnkalkutagi@gmail.com>
2025-02-28 23:27:10 +0530

    updated

--- Ch2/ex6.py
+++ Ch2/ex6.py
@@ -1,4 +1,4 @@
 #Write a python program to calculate the square of a number entered by the user.
 
 a = int(input("Enter a number: "))
-print(f"The square of {a} is {a * a}")
+print(f"The square of {a} is {a ** 2}")
