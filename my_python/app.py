d745297fba07030d67cf8c5697f1520d1011cc68
mankal-27 <mkalkutagi27@gmail.com>
2026-08-19 16:42:27 +0530

    updated new methodology

---
+++ my_python/app.py
@@ -0,0 +1,17 @@
+# This is a single line comment
+
+# This is a multiline comment
+# 1. Create User Name
+# 2. Print name
+# 3. Print Id
+
+# Store the final exam score
+x = 10
+
+x = 9  # Final exam score (Inline Comment)
+
+print(x)
+
+print("Hi Python")
+print('Hello Python')
+
