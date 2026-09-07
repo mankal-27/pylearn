c2bf94c0e0c0decb888e169ec12a6af7d92d7cbe
mankal-27 <mkalkutagi27@gmail.com>
2026-09-07 22:53:52 +0530

    started algo master

---
+++ pyAlgo/ch0/cart.py
@@ -0,0 +1,4 @@
+items = ["shirt", "shoes", "hat"]
+print(f"Your cart has {len(items)} items")
+
+print('cart total: ', 29.99 * len(items))
---
+++ pyAlgo/ch0/order_summary.py
@@ -0,0 +1,8 @@
+product_name = "Wireless Mouse"
+quantity = 2
+price = 19.99
+total = quantity * price
+
+print("Order Summary")
+print(product_name, "x", quantity)
+print("Total: ", total)
